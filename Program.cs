6418820ef97690df6622d91e1b1e003d1d3418a2
Vitor <vitor_mbn@hotmail.com>
2025-03-10 20:59:47 -0300

    Configuando teste de web

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Revisao de Wer API e EF");
+
+app.MapPost
 
 app.Run();
