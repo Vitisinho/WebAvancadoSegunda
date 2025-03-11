d22a0368e425aa55a1507fcbc7ae3a2af35bed50
Vitor <vitor_mbn@hotmail.com>
2025-03-10 21:29:57 -0300

    Configurando adicao de intens no banco de dados

--- Program.cs
+++ Program.cs
@@ -2,13 +2,17 @@ using API_REVISAO.Models;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Revisao de Wer API e EF");
 
-app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) => 
+app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto,
+[FromServices] AppDataContext ctx) => 
 {
-    return produto;
+    ctx.Produtos.Add(produto);
+    ctx.SaveChanges();
+    return Results.Created("", produto);
 });
 
 app.Run();
